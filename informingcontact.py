ab2805e11afe07df6ea2fa95787e20f0a361c175
Brodexe <catesbrody@gmail.com>
2025-04-13 21:52:57 -0500

    Added parsing text to graph, simulated.

--- informingcontact.py
+++ informingcontact.py
@@ -11,7 +11,7 @@ from copy import deepcopy
 
 # 2 ways that graphs can be saved in this code: Pinging Cytoscape via py4cytoscape, or saving a graph to gml file.
 # Change this to True if you want Cytoscape to be pinged from py4cytoscape
-ping_cytoscape = True
+ping_cytoscape = False
 
 # Change to True if you want to save to a gml file
 save_gmls = False
